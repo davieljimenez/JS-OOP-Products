c25b3d8bab0ef4c9f3ba6fab44a0becb7f831d53
Daviel Jimenez <Daviel050399@gmail.com>
2020-05-25 21:19:14 -0400

    Add class

---
+++ Scr/app.js
@@ -0,0 +1,21 @@
+class Producto {
+    constructor(Nombre, Precio, year){
+        this.Nombre = Nombre;
+        this.Precio = Precio;
+        this.year = year;
+    }
+}
+
+class UI{
+    agregarProducto(){
+
+    }
+
+    borrarProducto(){
+
+    }
+
+    verMensajes(){
+        
+    }
+}

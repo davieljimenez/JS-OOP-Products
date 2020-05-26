bca98aecb5bed82769778e524beeaa2caf30b220
Daviel Jimenez <Daviel050399@gmail.com>
2020-05-26 00:23:19 -0400

    Ver objetos en pantalla

--- Scr/app.js
+++ Scr/app.js
@@ -7,8 +7,20 @@ class Producto {
 }
 
 class UI{
-    agregarProducto(){
+    agregarProducto(producto) {
+        const listaDeProductos = document.getElementById("listaDeProductos");
+        const elemento = document.createElement('div');
+        elemento.innerHTML = `
+        <div class="card text-center mb-4">
+            <div class="card-body">
+                <strong>Nombre del producto</strong>: ${producto.Nombre} -
+                <strong>Precio del producto</strong>: ${producto.Precio} -
+                <strong>Año del producto</strong>: ${producto.year} 
+            </div>
+        </div>
+        `;
 
+    listaDeProductos.appendChild(elemento);
     }
 
     borrarProducto(){
@@ -16,6 +28,22 @@ class UI{
     }
 
     verMensajes(){
-        
+
     }
-}
+};
+
+//DOM EVENTOS + Obtener valores por ID
+document.getElementById("formularioProductos").addEventListener
+("submit", function(e){
+const nombreDelProducto = document.getElementById("Nombre").value;
+const precioDelProducto = document.getElementById("Precio").value;
+const yearDelProducto = document.getElementById("year").value;  
+
+const product = new Producto(nombreDelProducto, precioDelProducto, yearDelProducto);
+
+const ui =  new UI();
+ui.agregarProducto(product);
+
+e.preventDefault();
+
+});
